6eeaffb2c0e320f163073c657dd543774ce89786
Denis Kim <denis.kim666@gmail.com>
2022-07-27 21:24:13 +0600

    fill arrey function created

--- Program.cs
+++ Program.cs
@@ -12,5 +12,22 @@
     return newArrey;
 }
 
+string[] FillNewArrey(string[] array, string[]newArrey){
+
+    int counter = 0;
+
+    for(int i = 0; i < array.Length; i++){
+        if(array[i].Length <= 3){
+            newArrey[counter] = array[i];
+            counter++;
+        }
+    }
+
+    return newArrey;
+}
+
+
 string[] array = {"hello", "2", "world", ":-)", "hi", "help", "good", "we"};
 string[] newArr = CreateNewArray(array);
+
+FillNewArrey(array, newArr);
