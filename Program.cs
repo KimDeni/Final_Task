5dd7bcbefe3cf32440dc9901119f7e2fac50e39c
Denis Kim <denis.kim666@gmail.com>
2022-07-27 21:21:13 +0600

    second arrey created

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿string[] array = {"hello", "2", "world", ":-)", "hi", "help", "good", "we"};
+﻿string[] CreateNewArray(string []array){
+
+    int counter = 0;
+
+    for(int i = 0; i < array.Length; i++){
+        if(array[i].Length <= 3){
+            counter++;
+        }
+    }
+    string[] newArrey = new string[counter];
+
+    return newArrey;
+}
+
+string[] array = {"hello", "2", "world", ":-)", "hi", "help", "good", "we"};
+string[] newArr = CreateNewArray(array);
